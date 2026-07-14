9bc894aee8289f65abdff480e3e34af0a9807bc6
mustafa-jamali <mustafa.jamali1818@gmail.com>
2026-07-14 15:09:55 +0500

    Fix media URLs

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -18,9 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +28,7 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Development mode mein media files (images) show karne ke liye
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
