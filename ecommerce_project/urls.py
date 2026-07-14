8ba79b3d71d3d8658fe575b0ca18a3fcc15149f0
mustafa-jamali <mustafa.jamali1818@gmail.com>
2026-07-14 15:06:24 +0500

    Serve media files on Render

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,12 +31,6 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    ...
-]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
